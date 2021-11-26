bebad09d7a0c559292a5e8aac6cb8ec872b4eba6
rgupta00 <rgupta.mtech@gmail.com>
2021-11-26 12:06:50 +0530

    xml basics

---
+++ ivytrainingcode/ws1/xmlprocessing/src/com/dom/Reading.java
@@ -0,0 +1,46 @@
+package com.dom;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class Reading {
+
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		Document doc = dBuilder.parse("staff.xml");
+		//give me root
+		System.out.println(doc.getDocumentElement().getNodeName());
+		 
+		NodeList nList = doc.getElementsByTagName("staff");
+	 
+		System.out.println("----------------------------");
+	 
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+	 
+			Node nNode = nList.item(temp);
+	 
+			System.out.println("\nCurrent Element :" + nNode.getNodeName());
+	 
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+	 
+				Element eElement = (Element) nNode;
+	 
+				System.out.println("Staff id : " + eElement.getAttribute("id"));
+				System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
+				System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
+				System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
+				System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
+			}
+		}
+	}
+}
